0ea3d000fe7f6d164221e4c7db83da0680147a80
agentshield <anhsonleit@gmail.com>
2023-01-18 18:41:10 +0700

    Initial Commit

---
+++ ReciMeX/ReciMeXApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReciMeXApp.swift
+//  ReciMeX
+//
+//  Created by Anh Son Le on 18/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct ReciMeXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
